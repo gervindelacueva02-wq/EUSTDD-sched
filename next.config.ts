0893db41513115d1097ca236c812716faf191158
gervindelacueva <gervindelacueva02@gmail.com>
2026-03-03 10:58:57 +0800

    fix redeployment in render

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // Disable standalone for easier deployment on Render
+  // output: "standalone",
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
